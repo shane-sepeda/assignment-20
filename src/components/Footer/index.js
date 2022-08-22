813b45cfc520d2e7559e5dee8b29bc819b1176e8
shane-sepeda <shane.sepeda@icloud.com>
2022-08-22 02:18:01 -0500

    Updating JS

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>Developed by Shane Sepeda, 2022</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import Navigation from '../Navigation';
+
+function Header() {
+
+    const handlePageChange = (page) => setCurrentPage(page);
+    
+    return (
+        <>
+        <header>
+            <h1>
+                <a href="/portfolio">
+                </a>
+            </h1>
+            <Navigation currentPage={currentPage} handlePageChange={handlePageChange}></Navigation>
+        </header>
+        <main>
+        {renderPage()}
+        </main>
+        </>
+    );
+}
+
+export default Header;
