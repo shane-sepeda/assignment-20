2a84c019f842f92933bc1978e55b256a939d6048
shane-sepeda <shane.sepeda@icloud.com>
2022-08-23 23:28:08 -0500

    updated js

---
+++ src/components/Work/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Project from '../Project';
+
+function Work() {
+    return (
+        <section id='work' className='work'>
+            <h1>Work</h1>
+            <div >
+                <Project></Project>
+            </div>
+
+        
+        </section>
+    )
+}
+
+export default Work;
