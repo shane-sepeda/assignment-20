922d60c6a3e6f51388fcfabc3f2eb17703be8733
shane-sepeda <shane.sepeda@icloud.com>
2022-08-23 23:21:59 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import Background from '.src/assets/images/background.png';
-
+import Background from './assets/images/background.png';
 import Footer from './components/Footer';
 
 function App() {
